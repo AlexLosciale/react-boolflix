f5c5c190e9380261166bd99a38c02cc3d787659d
AlexLosciale <losciale17@gmail.com>
2025-03-05 19:48:29 +0100

    add:data

--- components/main.jsx
+++ components/main.jsx
@@ -1,20 +1,28 @@
 const Main = ({ movies }) => {
     return (
-        <div className="main">
-            <h1>Risultati</h1>
-            <div className="movies-grid">
-                {movies.map(movie => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
+        <>
+            <h1 className="lista-uno">RISULTATI FILM</h1>
+            <div className="main">
+                <div className="movies-grid">
+                    {movies.map(movie => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
 const MovieCard = ({ movie }) => {
+    const imageUrl = movie.poster_path 
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : 'https://via.placeholder.com/200';
+    
     return (
         <div className="movie-card">
+            <img src={imageUrl} alt={movie.title} />
             <h3>{movie.title}</h3>
+            <p>Uscita: {movie.release_date || 'Data non disponibile'}</p>
         </div>
     );
 };
