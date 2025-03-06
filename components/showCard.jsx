a35edc58dcdedb84424b9697175a7e7ae31addd9
AlexLosciale <losciale17@gmail.com>
2025-03-06 18:54:46 +0100

    add:stars

--- components/showCard.jsx
+++ components/showCard.jsx
@@ -3,12 +3,19 @@ const ShowCard = ({ movie }) => {
         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         : 'https://via.placeholder.com/200';
     
+    const maxStars = 5;
+    const rating = Math.min(Math.max(Math.floor(movie.vote_average / 2), 1), maxStars);
+    
     return (
         <div className="movie-card">
             <img src={imageUrl} alt={movie.title} />
             <h3>{movie.title}</h3>
             <p>Uscita: {movie.release_date || 'Data non disponibile'}</p>
-            <p>Voto: {Math.floor(movie.vote_average)}</p>
+            <p>
+                Voto: {Array.from({ length: maxStars }, (_, i) => (
+                    <span key={i}>{i < rating ? '★' : '☆'}</span>
+                ))}
+            </p>
         </div>
     );
 };
--- components/showCardTv.jsx
+++ components/showCardTv.jsx
@@ -3,12 +3,19 @@ const ShowCardTv = ({ movie }) => {
         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         : 'https://via.placeholder.com/200';
     
+    const maxStars = 5;
+    const rating = Math.min(Math.max(Math.floor(movie.vote_average / 2), 1), maxStars);
+    
     return (
         <div className="movie-card">
-            <img src={imageUrl} alt={movie.title} />
+            <img src={imageUrl} alt={movie.original_name} />
             <h3>{movie.original_name}</h3>
             <p>Uscita: {movie.first_air_date || 'Data non disponibile'}</p>
-            <p>Voto: {Math.floor(movie.vote_average)}</p>
+            <p>
+                Voto: {Array.from({ length: maxStars }, (_, i) => (
+                    <span key={i}>{i < rating ? '★' : '☆'}</span>
+                ))}
+            </p>
         </div>
     );
 };
